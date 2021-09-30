dbcf21d6d061cce4fc010fea6b8f775430b4ca06
Kaare Veggerby Sandbøl <kaar1498@edu.ucl.dk>
2021-09-30 12:41:41 +0200

    Test WIP

---
+++ LandonHotelTests/BookingServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace LandonHotelTests
+{
+    public class BookingServiceTests
+    {
+        [Fact]
+        public void Should_Calculuate_Booking_Cost_True()
+        {
+
+        }
+    }
+}
--- LandonHotelTests/RoomServiceTests.cs
+++ LandonHotelTests/RoomServiceTests.cs
@@ -5,10 +5,22 @@ namespace LandonHotelTests
 {
     public class RoomServiceTests
     {
+        //TODO: Class setup
+        // Inline
+        //Negative testing
+        // Moq the services
+        // Gets called once
+        // Controller
+        // Repository
+
+
         [Fact]
         public void Should_Get_All_Rooms_True()
         {
 
+
+
+            Assert.Equal(null, null);
         }
     }
 }
