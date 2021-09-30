06a0e5a86229e7c17a410058cc8bf959a638c372
Kaare Veggerby Sandbøl <kaar1498@edu.eal.dk>
2021-09-30 12:23:05 +0200

    Test init

---
+++ LandonHotelTests/RoomServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LandonHotelTests
+{
+    public class RoomServiceTests
+    {
+        [Fact]
+        public void Should_Get_All_Rooms_True()
+        {
+
+        }
+    }
+}
